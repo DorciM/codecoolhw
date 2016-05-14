5fc0cd8f29fcd2ba6d1671e34b7c7d0001efd5bc
DorciM <dora.medgyasszay@gmail.com>
2016-05-14 20:48:28 +0200

    Update hello_world_cc.py

--- hello_world_cc.py
+++ hello_world_cc.py
@@ -2,7 +2,7 @@
 #If there's no name given, it welcomes the World ("Hello World").
 
 import sys
-
+# I used sys.argv to check if anything (name) is entered other than the name of the program
 def Hello():
 	if len(sys.argv) > 1:
 		print("Hello ",sys.argv[1])
@@ -10,3 +10,4 @@ def Hello():
 		print("Hello World")
 
 Hello()
+
